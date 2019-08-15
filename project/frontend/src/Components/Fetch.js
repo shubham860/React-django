a0221dc1383c12bba376ae9876957358b3a82239
shubham860 <shubham.chauhan860@gmail.com>
2019-08-16 04:45:37 +0530

    commit-4

--- project/frontend/src/Components/Fetch.js
+++ project/frontend/src/Components/Fetch.js
@@ -17,35 +17,41 @@ class Fetch extends React.Component {
        .get(`/employee/`)
        .then(response =>{
            console.log(response.data)
-           var array = response.data
-           this.setState({
-             firstname : array[0].firstname,
-             lastname : array[0].lastname
-           })
-           })
+           var x = response.data
+          for(let i=0;i<x.length;i++){
+             this.setState({
+               firstname : x[i].firstname,
+               lastname : x[i].lastname
+             })}
+         })
        .catch(error => {
            alert('Error in fetching data')
          })
   }
   render () {
+    const {firstname,lastname} = this.state
     return(
-     <div>
-      <button onClick={this.fetch}>Fetch</button>
-        <table>
-          <thead>
-            <tr>
-              <th>Firstname</th>
-              <th>Lastname</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{firstname}</td>
-              <td>{lastname}</td>
-            </tr>]
-          </tbody>  
-        </table>
-       </div>
+     <div class='container'><br/><br/>
+      <div class='row'>
+        <a class="waves-effect waves-light btn-small " onClick={this.fetch}>Fetch</a>
+      </div><br/><br/>
+        <div class='row'>
+          <table class="responsive-table centered highlight">
+            <thead>
+              <tr>
+                <th>Firstname</th>
+                <th>Lastname</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{firstname}</td>
+                <td>{lastname}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
   )}
 }
 
